752ebe68ba986a906a28300435e1ee9f0e4570df
CelestialEcho <152692161+CelestialEcho@users.noreply.github.com>
2023-12-03 12:36:57 +0300

    Update crypto_price_parser.py
    fixed bugs, added logs(?)

--- crypto_price_parser.py
+++ crypto_price_parser.py
@@ -5,6 +5,7 @@ import datetime
 import pytz
 import time
 import json
+import requests
 
 headers_randomuseragents = ['Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0',
                             'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:112.0) Gecko/20100101 Firefox/112.0',
@@ -13,7 +14,8 @@ headers_randomuseragents = ['Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0)
                             'Mozilla/5.0 (Macintosh; Intel Mac OS X 13.3; rv:102.0) Gecko/20100101 Firefox/102.0',
                             'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 OPR/97.0.4719.17']
 headers_randomreferer = ['https://www.yahoo.com', 'https://www.duckduckgo.com', 'https://www.google.com',
-                         'https://www.bing.com', 'https://scholar.google.com']
+                         'https://www.bing.com']
+
 
 chrome_options = Options()
 chrome_options.add_argument('--headless')
@@ -22,6 +24,7 @@ chrome_options.add_argument('--disable-gpu')
 
 random_user_agent = random.choice(headers_randomuseragents)
 random_referer = random.choice(headers_randomreferer)
+print(f'user-agent = {random_user_agent}\nreferer = {random_referer}')
 
 
 chrome_options.add_argument(f'user-agent={random_user_agent}')
@@ -29,20 +32,37 @@ chrome_options.add_argument(f'referer={random_referer}')
 
 driver = webdriver.Chrome(options=chrome_options)
 
-
 url = 'https://ru.investing.com/crypto/currency-pairs'
 main_data = {}
+
+
+headers = {
+    'User-Agent': random_user_agent,
+    'Referer': random_referer,
+}
+request = requests.get(url, headers=headers)
+request = str(request)
+request = request.replace('<Response [', '').replace(']>', '')
+
+if request == '200':
+    print('Successful! (Response 200)')
+elif request == '404':
+    print("Can't connect to website! (Response 404)")
+else:
+    print(f"Response code: {request} wasn't expected!")
+
+time.sleep(random.uniform(31, 67))
 
 
 def __bitcoin__():
     driver.get(url)
     time.sleep(random.uniform(1, 30))
     bitcoin = driver.find_element('css selector', 'td.pid-1057391-last')
-    print(bitcoin.text)
+    print(f'Bitcoin price (USD): {bitcoin.text}')
     time_sleep = random.uniform(1, 30)
     time.sleep(time_sleep)
     bitcoin = str(bitcoin.text)
-    bitcoin = bitcoin.replace(',', '')
+    bitcoin = bitcoin.replace('.', '').replace(',', '.')
     currency = float(bitcoin)
 
     return currency
@@ -60,9 +80,8 @@ def add_dict(n1):
 while True:
     n = __bitcoin__()
     answer = add_dict(n)
-    print(answer)
-
+    print(f'Added to dictionary:\n{answer}')
     with open("prices.json", 'w') as json_file:
         json.dump(main_data, json_file)
-
     time.sleep(random.uniform(60, 120))
+
